394ce800e061e6355abde4649e50602faa4a4082
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2021-11-07 11:42:17 -0800

    Update assignment2.js

--- assignment2.js
+++ assignment2.js
@@ -80,18 +80,18 @@ class Base_Scene extends Scene {
   }
 
   draw_sun(context, program_state) {
-    const scale_factor = 30;
+    const scale_factor = 5;
     const sun_color = color(1, 1, 1, 1);
 
-    const sun_model_transform = Mat4.translation(-30, 50, 50).times(Mat4.scale(
+    const sun_model_transform = Mat4.translation(-10, 30, 30).times(Mat4.scale(
       scale_factor,
       scale_factor,
       scale_factor
     ).times(Mat4.identity()));
 
-    const light_position = vec4(-30, 50, 50, 1);
+    const light_position = vec4(-10, 50, 50, 1);
     program_state.lights = [
-      new Light(light_position, sun_color, 1000 ** scale_factor),
+      new Light(light_position, sun_color, 1000 ** 30),
     ];
 
     this.shapes.sphere_4.draw(
